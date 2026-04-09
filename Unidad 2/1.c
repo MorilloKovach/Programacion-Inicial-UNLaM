a5bc97d25504ab3f1b089a29229645845ea2da8d
kovac <xxsmating@gmail.com>
2026-04-09 14:43:24 -0300

    Agregar unidad 2

---
+++ Unidad 2/1.c
@@ -0,0 +1,8 @@
+/*
+Confeccionar un programa que pueda ingresar 2 números enteros y calcule e informe con
+mensajes aclaratorios la suma, el producto, el cociente y el resto. Precondición: el segundo
+número es distinto de cero.
+*/
+
+#include<stdio.h>
+

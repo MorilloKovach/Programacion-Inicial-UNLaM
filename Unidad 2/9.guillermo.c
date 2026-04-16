c48f5a9bba2168e5f6c170cbf4cced30380a43ff
kovac <xxsmating@gmail.com>
2026-04-16 08:17:44 -0300

    Agregando el ejercicio de la unidad 2 a la forma de guillermo. Porque tiene ganas de joder :P

---
+++ Unidad 2/9.guillermo.c
@@ -0,0 +1,18 @@
+/*
+ Se ingresa un número entero de 3 cifras. Descomponerlo en unidad, decena y centena e
+informarlos con mensaje aclaratorio.
+*/
+
+#include<stdio.h>
+//tiene ganas de romper las bolas este profe, es lo mismo.
+
+int main(){
+    int numero;
+    printf("Digite el numero de 3 cifras: ");
+    scanf("%d",&numero);
+    printf("La cantidad de unidades es de: %d",numero%10);
+    numero/=10;
+    printf("La cantidad de decenas es de: %d",numero%10);
+    numero/=10;
+    printf("La cantidad de centenas es de: %d",numero%10);
+}

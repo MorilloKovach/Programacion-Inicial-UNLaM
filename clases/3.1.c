5a6c63e5944535b6c444afe169e92d2fec9290cf
kovac <xxsmating@gmail.com>
2026-04-23 09:48:14 -0300

    Ejercicio pre-recreo

--- clases/3.1.c
+++ clases/3.1.c
@@ -3,7 +3,7 @@
 #include<stdbool.h>
 
 int main(){
-    int cantidadPrimos, contadorPrimos, numero = 2;
+    int cantidadPrimos, contadorPrimos, i, numero=2;
     bool band=true, esPrimo;
 
     do{
@@ -13,7 +13,25 @@ int main(){
             band=false;
         }
         else{
-            
+            printf("Por favor, ingrese una cantidad valida. ");
+            band = true;
         }
+    }while(cantidadPrimos <= 0);
+    contadorPrimos = 0;
+    while(contadorPrimos < cantidadPrimos){
+        esPrimo = true;
+        i = 2;
+        while(esPrimo && i < numero){
+            if(numero%i==0){
+                esPrimo = false;
+            }
+            i++;
+        }
+        if(esPrimo){
+            contadorPrimos++;
+            printf("\nEl primo es %d",numero);
+        }
+        numero++;
     }
+    printf("\n");
 }
